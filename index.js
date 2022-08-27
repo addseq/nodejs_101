4429a42a9074e711a17f375a533139823f744048
addseq <addseq@users.noreply.github.com>
2022-08-27 03:25:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// This is a comment. Only humans see me.
+// First data type: string
+// collection of characters
+console.log(typeof "hello")
+
+// 4 steps of programming
+// 1. data types <> vocab
+// 2. methods <> rules for part of speech
+// 3. logic
+// 4. library
